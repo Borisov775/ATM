2daaf75756391e65e57de5a52b51ff260559ea55
Assel Romanova <asel-romanova@mail.ru>
2021-11-11 17:11:37 +0300

    balance fragment

--- app/src/main/java/ru/asselinux/banknotestest/ui/BalanceFragment.kt
+++ app/src/main/java/ru/asselinux/banknotestest/ui/BalanceFragment.kt
@@ -1,12 +1,6 @@
 package ru.asselinux.banknotestest.ui
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import ru.asselinux.banknotestest.R
 
-class BalanceFragment : Fragment(R.layout.fragment_balance) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-}
+class BalanceFragment : Fragment(R.layout.fragment_balance)
